b26739e836b001d9b57eba126fcfe84e70a69165
Imre Gábor <gabor@aut.bme.hu>
2019-06-11 11:18:38 +0200

    add project generated with Spring Initializr

---
+++ spring-thymeleaf-todo/src/main/java/hu/autsoft/hwsw/spring/web/todo/SpringThymeleafTodoApplication.java
@@ -0,0 +1,13 @@
+package hu.autsoft.hwsw.spring.web.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafTodoApplication.class, args);
+	}
+
+}
